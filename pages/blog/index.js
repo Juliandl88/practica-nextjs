5ec54907031b035eafb249afc0e70d5185f995e7
Julian <julian_delisio@hotmail.com>
2022-08-07 23:20:25 -0300

    conectando api

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,12 +2,34 @@
 import Link from "next/link";
 import Layout from '../../components/Layout';
 
-export default function Home() {
+export default function index({data}) {
   return (
     <div >
       <Layout href="/blog/primer-post">
       <h1>INICIO</h1>
+      {
+        data.map(({ id, title, body}) => (
+       <div key={id}>
+          <h3>{id} - {title}</h3>
+          <p>{body}</p>
+       </div>
+        ))
+      }
       </Layout>
     </div>
   )
+}
+
+export async function getStaticProps(){
+  try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      return {
+        props: {
+          data
+        }
+      }
+  } catch (error) {
+      console.error(error);
+  }
 }
